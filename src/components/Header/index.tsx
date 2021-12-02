a485ef1b29058bc71a5cbdd7e0932117b1f77fc5
Garoze <marcelogaroze@hotmail.com>
2021-12-02 18:59:46 -0300

    feat: Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import style from "./styles.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={style.headerContainer}>
+      <div className={style.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a href="#" className={style.active}>
+            Home
+          </a>
+          <a href="#">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
