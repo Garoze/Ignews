34dd5f2fd401e3d7e8e3e7ec5a0968c6a1b9305b
Garoze <marcelogaroze@hotmail.com>
2021-12-02 19:44:34 -0300

    feat: Page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
 
+import style from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello World!</h1>
+      <main className={style.contentContainer}>
+        <section className={style.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the plubications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
