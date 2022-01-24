5794cc29b59dd6f40a91a49f105d5ff783deaecb
Garoze <marcelogaroze@hotmail.com>
2022-01-24 17:43:45 -0300

    feat: Load the post from prismic
    Added the [slug].tsx file to the /pages/posts.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
+import Link from "next/link";
 
 import Prismic from "@prismicio/client";
 import { RichText } from "prismic-dom";
@@ -28,11 +29,13 @@ const Posts = ({ posts }: PostsProps) => {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a key={post.slug} href="">
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
